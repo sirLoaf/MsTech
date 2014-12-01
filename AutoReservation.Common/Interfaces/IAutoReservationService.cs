845506f87f7423bd344694165e0ff0bbfe2b5106
sirLoaf <flaib@hsr.ch>
2014-12-01 13:31:22 +0100

    IAutoReservationService

--- AutoReservation.Common/Interfaces/IAutoReservationService.cs
+++ AutoReservation.Common/Interfaces/IAutoReservationService.cs
@@ -6,5 +6,24 @@ namespace AutoReservation.Common.Interfaces
 {
     public interface IAutoReservationService
     {
+        void UpdateAuto(AutoDto changed, AutoDto old);
+        void UpdateKunde(KundeDto changed, KundeDto old);
+        void UpdateReservation(ReservationDto changed, ReservationDto old);
+
+        AutoDto insertAuto(AutoDto auto);
+        KundeDto insertKunde(KundeDto kunde);
+        ReservationDto insertReservation(ReservationDto reservation);
+
+        AutoDto deletAuto(AutoDto auto);
+        KundeDto deleteKunde(KundeDto kunde);
+        ReservationDto deleteReservation(ReservationDto reservation);
+
+        List<AutoDto> getAutos();
+        List<KundeDto> getKunden();
+        List<ReservationDto> getReservationen();
+
+        AutoDto getAuto(int index);
+        KundeDto getKunder(int index);
+        ReservationDto getReservation(int index);
     }
 }
