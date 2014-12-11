1342ee2ee57f20e2bd51a9bc6837ab64154a8718
sirLoaf <flaib@hsr.ch>
2014-12-11 14:55:18 +0100

    removed unused usings

--- AutoReservation.Ui/Factory/Creator.cs
+++ AutoReservation.Ui/Factory/Creator.cs
@@ -1,10 +1,6 @@
 ﻿using AutoReservation.Common.Interfaces;
 using AutoReservation.Ui.Properties;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 namespace AutoReservation.Ui.Factory 
 { 
     public abstract class Creator 
--- AutoReservation.Ui/ViewModels/ReservationViewModel.cs
+++ AutoReservation.Ui/ViewModels/ReservationViewModel.cs
@@ -1,10 +1,8 @@
 ﻿using AutoReservation.Common.DataTransferObjects;
-using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace AutoReservation.Ui.ViewModels
